b72d90709e5d5098004890cc3dfb9fcf17768239
thomas <thomaalanstevens@gmail.com>
2022-07-12 04:04:25 -0700

    Finished footer styling, changed out main carousel imgs with embbed youtube videos, coded autoplay on hover and mute functions.

--- script.js
+++ script.js
@@ -62,3 +62,61 @@ var swiper1 = new Swiper(".mySwiper", {
         },
     }
   });
+
+//   Dark mode button
+document.querySelector('.fa-circle-half-stroke').addEventListener('click', darkMode)
+
+function darkMode(){
+    document.querySelector('#search').classList.toggle("dark-mode") 
+    document.querySelector('body').classList.toggle("dark-mode");
+    document.querySelector('nav').classList.toggle("dark-mode");
+    document.querySelector('.fade-left').classList.toggle("dark-mode");
+    document.querySelectorAll('.fade-left-minor').forEach(fade => fade.classList.toggle("dark-mode"))
+    document.querySelector('.fade-right').classList.toggle("dark-mode");
+    document.querySelectorAll('.fade-right-minor').forEach(fade => fade.classList.toggle("dark-mode"))
+    document.querySelectorAll('.swiper-slide').forEach(slide => slide.classList.toggle("dark-mode"))
+    document.querySelectorAll('button').forEach(button => button.classList.toggle("dark-mode"))
+    document.querySelectorAll('a').forEach(a => a.classList.toggle("dark-mode"))
+    document.querySelector('header').classList.toggle("dark-mode");
+}
+
+// Autoplay muted video on mouse hover using youtube api
+document.querySelectorAll('iframe').forEach(iframe => iframe.addEventListener('mouseenter', playVideo))
+
+function playVideo(){
+    console.log(this)
+    this.src = this.src.replace('&autoplay=0', '&autoplay=1')
+    
+}
+
+document.querySelectorAll('iframe').forEach(iframe => iframe.addEventListener('mouseleave', stopVideo))
+
+function stopVideo(){
+    this.src = this.src.replace('&autoplay=1', '&autoplay=0')
+}
+
+// Mutes all videos on played during hover
+var tag = document.createElement('script');
+
+tag.src = "//www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('vid1', {
+        events: {
+            'onReady': onPlayerReady
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    player.mute();
+}
+
+// Hides info on hover during play
+
+
+
